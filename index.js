6929aab070ea5d7f960f5dd3d0a5b024b885160b
Marvin Chandra <marvinchan05@gmail.com>
2023-08-01 11:00:09 +0700

    Add reset button

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const countDisplay = document.querySelector("#count");
 function increment() {
     countDisplay.innerHTML = ++count;
 }
+
+function resetCount() {
+    count = 0;
+    countDisplay.innerHTML = count;
+}
